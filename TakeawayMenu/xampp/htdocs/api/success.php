022529a38b92b009e3a5bd9411c8bc5fdef66e8d
sati5M <70115567+sati5M@users.noreply.github.com>
2025-10-03 03:58:42 +0100

    Remove unused stripe key

--- TakeawayMenu/xampp/htdocs/api/success.php
+++ TakeawayMenu/xampp/htdocs/api/success.php
@@ -8,7 +8,7 @@ $app = require __DIR__ . '/../lib/bootstrap.php';
 
 
 
-$stripe = new \Stripe\StripeClient('sk_test_51PD4xIJhJMUNCOjAPA1pRUwTD1O8kKOvxbStd1G7Tml9vOEaegyg8uTYnllnmgyw9k8tEDlFp0mS8nnrNsxKbtEu00FaUNoY4M');
+$stripe = new \Stripe\StripeClient('STRIPE_KEY');
 $sessionData = $stripe->checkout->sessions->retrieve($_GET['provider_session_id'], []);
 
 if ($sessionData->payment_status === 'paid') {
